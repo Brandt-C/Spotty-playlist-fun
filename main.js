892183adc788392ffedef580fb9653764db4f894
Brandt-C <brandt.carlson11@gmail.com>
2022-03-25 19:52:22 -0500

    getting somewhere- cards load

--- main.js
+++ main.js
@@ -15,9 +15,9 @@ const getAuth = async () => {
 }
 
 
-const getSong = async (songname, artist) => {
+const getSong = async (song, artist) => {
     const token = await getAuth();
-    let response = await fetch(`https://api.spotify.com/v1/search/?type=track&q=track:${songname}+artist:${artist}`,
+    let response = await fetch(`https://api.spotify.com/v1/search/?type=track&q=track:${song}+artist:${artist}`,
         {
             method: 'GET',
             headers: {
@@ -27,9 +27,48 @@ const getSong = async (songname, artist) => {
         });
     console.log(response);
     let data = await response.json();
+    if (response.status != 200) {
+        console.log("\nSomething went wrong here.  Maybe it was you, maybe it was us. . . either way, Try Again!")
+        return
+    }
     console.log(data.tracks.items[0]);
     return data.tracks.items[0]
 }
 
-getSong('bang', 'ajr');
+// getSong('bang', 'ajr');
 
+
+let loadSong = async (song, artist) => {
+    let data = await getSong(song, artist);
+    console.log(data);
+    if (data.preview_url == null){
+        let lol_card = `<div class="card" style="max-width: 540px">
+        <img src="/static/images/ny.jpg" class="card-img-top" alt="An artist that doesn't do spotify">
+        <div class="card-body">
+            <h5 class="card-song">Song?  No song. . .</h5>
+            <p class="card-artist">This artist might not like spotify (just like the dude in the pic).</p>
+            <p class="card-album">At any rate there's no audio so this won't be added. . . </p>
+        </div>
+    </div>`;
+    document.getElementById('songcard').insertAdjacentHTML('afterbegin', lol_card);
+    }
+    let new_card = `<div class="card" style="max-width: 540px">
+    <img src="${data.album.images[0].url}" class="card-img-top" alt="Album cover">
+    <div class="card-body">
+        <h5 class="card-song">Track- ${data.name}</h5>
+        <p class="card-artist">Artist- ${data.artists[0].name}</p>
+        <p class="card-album">Album- ${data.album.name}</p>
+    </div>
+    </div>`;
+    document.getElementById('songcard').insertAdjacentHTML('afterbegin', new_card);
+}
+
+let sform = document.querySelector('#sform');
+sform.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let song = event.path[0][0].value
+    let artist = event.path[0][1].value
+    console.log(song, artist);
+    loadSong(song, artist); 
+    sform.reset();
+});
